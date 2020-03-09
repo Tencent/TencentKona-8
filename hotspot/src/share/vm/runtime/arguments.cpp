1db65160f6cf6dd1a3946401ec1ccd5fe01fdb02
daisyqhuang <daisyqhuang@tencent.com>
2020-03-09 23:51:34 +0800

    Revert "first commit from openjdk jdk8u222-ga"
    This reverts commit 27706d31c9e00a17deb8c01cdbda35442d72f498, which caused some commits missing.
    Conflicts:
    	THIRD_PARTY_README
    	common/autoconf/generated-configure.sh
    	corba/THIRD_PARTY_README
    	hotspot/THIRD_PARTY_README
    	hotspot/src/cpu/ppc/vm/vm_version_ppc.cpp
    	hotspot/src/share/vm/opto/connode.cpp
    	hotspot/src/share/vm/runtime/stubRoutines.cpp
    	hotspot/test/TEST.groups
    	hotspot/test/compiler/c2/Test8217359.java
    	hotspot/test/runtime/RedefineTests/RedefineDoubleDelete.java
    	jaxp/THIRD_PARTY_README
    	jaxws/THIRD_PARTY_README
    	jdk/THIRD_PARTY_README
    	jdk/src/share/native/sun/font/freetypeScaler.c
    	jdk/src/solaris/native/sun/security/smartcardio/MUSCLE/COPYING
    	jdk/test/TEST.groups
    	jdk/test/sun/security/ec/SignatureDigestTruncate.java
    	langtools/THIRD_PARTY_README
    	langtools/test/Makefile
    	langtools/test/ProblemList.txt
    	langtools/test/TEST.groups
    	nashorn/THIRD_PARTY_README
    
    Co-authors:
    Co-authored-by: Daisyq Huang<daisyqhuang@tencent.com>

--- hotspot/src/share/vm/runtime/arguments.cpp
+++ hotspot/src/share/vm/runtime/arguments.cpp
@@ -573,8 +573,7 @@ char* SysClassPath::add_jars_to_path(char* path, const char* directory) {
 
   /* Scan the directory for jars/zips, appending them to path. */
   struct dirent *entry;
-  char *dbuf = NEW_C_HEAP_ARRAY(char, os::readdir_buf_size(directory), mtInternal);
-  while ((entry = os::readdir(dir, (dirent *) dbuf)) != NULL) {
+  while ((entry = os::readdir(dir)) != NULL) {
     const char* name = entry->d_name;
     const char* ext = name + strlen(name) - 4;
     bool isJarOrZip = ext > name &&
@@ -588,7 +587,6 @@ char* SysClassPath::add_jars_to_path(char* path, const char* directory) {
       FREE_C_HEAP_ARRAY(char, jarpath, mtInternal);
     }
   }
-  FREE_C_HEAP_ARRAY(char, dbuf, mtInternal);
   os::closedir(dir);
   return path;
 }
@@ -3499,14 +3497,12 @@ static bool has_jar_files(const char* directory) {
   if (dir == NULL) return false;
 
   struct dirent *entry;
-  char *dbuf = NEW_C_HEAP_ARRAY(char, os::readdir_buf_size(directory), mtInternal);
   bool hasJarFile = false;
-  while (!hasJarFile && (entry = os::readdir(dir, (dirent *) dbuf)) != NULL) {
+  while (!hasJarFile && (entry = os::readdir(dir)) != NULL) {
     const char* name = entry->d_name;
     const char* ext = name + strlen(name) - 4;
     hasJarFile = ext > name && (os::file_name_strcmp(ext, ".jar") == 0);
   }
-  FREE_C_HEAP_ARRAY(char, dbuf, mtInternal);
   os::closedir(dir);
   return hasJarFile ;
 }
@@ -3588,8 +3584,7 @@ static bool check_endorsed_and_ext_dirs() {
   if (dir != NULL) {
     int num_ext_jars = 0;
     struct dirent *entry;
-    char *dbuf = NEW_C_HEAP_ARRAY(char, os::readdir_buf_size(extDir), mtInternal);
-    while ((entry = os::readdir(dir, (dirent *) dbuf)) != NULL) {
+    while ((entry = os::readdir(dir)) != NULL) {
       const char* name = entry->d_name;
       const char* ext = name + strlen(name) - 4;
       if (ext > name && (os::file_name_strcmp(ext, ".jar") == 0)) {
@@ -3608,7 +3603,6 @@ static bool check_endorsed_and_ext_dirs() {
         }
       }
     }
-    FREE_C_HEAP_ARRAY(char, dbuf, mtInternal);
     os::closedir(dir);
     if (num_ext_jars > 0) {
       nonEmptyDirs += 1;
