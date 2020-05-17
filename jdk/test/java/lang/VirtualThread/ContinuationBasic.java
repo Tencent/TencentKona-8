62c0c19272ce000e88f4a321b3773f156bcae59c
kalinshi <kalinshi@tencent.com>
2020-05-17 10:55:13 +0800

    Feature(KonaFiber):Port Loom interface and make it run with JKU asym coroutine
    1. Add Continuation/VirtualThread declaration
    2. directly use aymcoroutine as Continuation
    3. Add simpel test cases
    
    --task=7254255
    
    Contributed-by: kalinshi
    
    	modified:   jdk/src/share/classes/com/sun/jndi/dns/DnsContext.java
    	modified:   jdk/src/share/classes/com/sun/jndi/ldap/LdapCtx.java
    	new file:   jdk/src/share/classes/java/lang/Continuation.java
    	new file:   jdk/src/share/classes/java/lang/ContinuationScope.java
    	modified:   jdk/src/share/classes/java/lang/Thread.java
    	new file:   jdk/src/share/classes/java/lang/VirtualThread.java

---
+++ jdk/test/java/lang/VirtualThread/ContinuationBasic.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2003, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @run testng ContinuationBasic 
+ * @summary Basic test for continuation, test create/run/yield/resume/stop
+ */
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ContinuationBasic {
+    public static int value = 0;
+    public static void foo() {
+        System.out.println("foo");
+        value++;
+    }
+
+    @Test
+    public static void test_start() {
+        ContinuationScope scope = new ContinuationScope("scope");
+        value = 0;
+        Continuation cont = new Continuation(scope, () -> {
+           foo();
+        });
+        cont.run();
+        assertEquals(value, 1);
+        System.out.println(value);
+    }
+
+    @Test
+    public static void test_yield() {
+        ContinuationScope scope = new ContinuationScope("scope");
+        value = 0;
+        Continuation cont = new Continuation(scope, () -> {
+           System.out.println("enter cont");
+           foo();
+           Continuation.yield();
+           foo();
+           System.out.println("exit cont");
+        });
+        assertEquals(value, 0);
+        System.out.println(value);
+        cont.run();
+        System.out.println(value);
+        assertEquals(value, 1);
+        value += 2;
+        cont.run();
+        System.out.println(value);
+        assertEquals(value, 4);
+    }
+}
+
