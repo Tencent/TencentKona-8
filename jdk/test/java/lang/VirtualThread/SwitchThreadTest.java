79ebac1f0cae1fb183422bb6d7073775ffc48218
emoryzheng <emoryzheng@tencent.com>
2020-06-05 10:50:00 +0800

    FIX(KonaFiber): Add assertEquals to UT
    1. Add assert to make sure the behavior of testcase is expected
    2. Replace tab with spaces
    
    --task=7254255
    
    Contributed-by:emoryzheng
    
        modified:   jdk/test/java/lang/VirtualThread/MultiContinuation.java
        modified:   jdk/test/java/lang/VirtualThread/PinTest.java
        modified:   jdk/test/java/lang/VirtualThread/SimpleContinuation.java
        modified:   jdk/test/java/lang/VirtualThread/SimpleVT.java
        modified:   jdk/test/java/lang/VirtualThread/SwitchThreadTest.java

--- jdk/test/java/lang/VirtualThread/SwitchThreadTest.java
+++ jdk/test/java/lang/VirtualThread/SwitchThreadTest.java
@@ -25,6 +25,10 @@
  * @test
  * @summary Basic test for continuation, test create/run/yield/resume/stop
  */
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
 public class SwitchThreadTest {
     static long count = 0;
     static ContinuationScope scope = new ContinuationScope("test");
@@ -46,15 +50,16 @@ public class SwitchThreadTest {
     static void foo() throws Exception {
         Runnable target = new Runnable() {
             public void run() {
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("before yield");
+                System.out.println("before yield, Continuation run in " + Thread.currentThread().getName());
+                assertNotEquals(Thread.currentThread().getName(), "main");
                 Continuation.yield(scope);
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("resume yield");
+                System.out.println("resume yield, Continuation run in " + Thread.currentThread().getName());
+                assertNotEquals(Thread.currentThread().getName(), "main");
             }
         };
         Continuation cont = new Continuation(scope, target);
         System.out.println("Continuation create in " + Thread.currentThread().getName());
+        assertEquals(Thread.currentThread().getName(), "main");
         Thread thread = new Thread(){
             public void run() {
                 cont.run();
@@ -63,23 +68,22 @@ public class SwitchThreadTest {
         };
         thread.start();
         thread.join();
-        //cont.run();
-        //cont.run();
     }
 
     static void bar() throws Exception {
         Runnable target = new Runnable() {
             public void run() {
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("before yield");
+                System.out.println("before yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "bar-thread-0");
                 Continuation.yield(scope);
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("resume yield");
+                System.out.println("resume yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "main");
             }
         };
         Continuation cont = new Continuation(scope, target);
         System.out.println("Continuation create in " + Thread.currentThread().getName());
-        Thread thread = new Thread(){
+        assertEquals(Thread.currentThread().getName(), "main");
+        Thread thread = new Thread("bar-thread-0"){
             public void run() {
                 cont.run();
             }
@@ -87,24 +91,25 @@ public class SwitchThreadTest {
         thread.start();
         thread.join();
         cont.run();
-        //cont.run();
     }
+
     // create in other thread and run in main
     static Continuation internal;
     static void qux() throws Exception {
         Runnable target = new Runnable() {
             public void run() {
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("before yield");
+                System.out.println("before yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "main");
                 Continuation.yield(scope);
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("resume yield");
+                System.out.println("resume yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "main");
             }
         };
-        Thread thread = new Thread(){
+        Thread thread = new Thread("qux-thread-0"){
             public void run() {
                 Continuation cont = new Continuation(scope, target);
                 System.out.println("Continuation create in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "qux-thread-0");
                 internal = cont;
             }
         };
@@ -118,59 +123,61 @@ public class SwitchThreadTest {
     static void baz() throws Exception {
         Runnable target = new Runnable() {
             public void run() {
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("before yield");
+                System.out.println("before yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "main");
                 Continuation.yield(scope);
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("resume yield");
+                System.out.println("resume yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "baz-thread-0");
             }
         };
-        Thread thread = new Thread(){
+        Thread thread = new Thread("baz-thread-0"){
             public void run() {
                 Continuation cont = new Continuation(scope, target);
                 System.out.println("Continuation create in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "baz-thread-0");
                 internal = cont;
                 try {
-                	Thread.sleep(2000);
+                    Thread.sleep(2000);
                 } catch (Exception e) {
                 }
-				cont.run();
+                cont.run();
             }
         };
         thread.start();
         Thread.sleep(1000);
         internal.run();
-		thread.join();
+        thread.join();
     }
 
 
-	static void switch3threads() throws Exception {
+    static void switch3threads() throws Exception {
         Runnable target = new Runnable() {
             public void run() {
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("before yield");
+                System.out.println("before yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "second-thread");
                 Continuation.yield(scope);
-                System.out.println("Continuation run in " + Thread.currentThread().getName());
-                System.out.println("resume yield");
+                System.out.println("resume yield, Continuation run in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "third-thread");
             }
         };
-        Thread thread = new Thread(){
+        Thread thread = new Thread("first-thread"){
             public void run() {
                 Continuation cont = new Continuation(scope, target);
                 System.out.println("Continuation create in " + Thread.currentThread().getName());
+                assertEquals(Thread.currentThread().getName(), "first-thread");
                 internal = cont;
             }
         };
         thread.start();
         thread.join();
-        Thread thread1 = new Thread(){
+        Thread thread1 = new Thread("second-thread"){
             public void run() {
-				internal.run();
+                internal.run();
             }
         };
         thread1.start();
         thread1.join();
-        Thread thread2 = new Thread(){
+        Thread thread2 = new Thread("third-thread"){
             public void run() {
                 internal.run();
             }
