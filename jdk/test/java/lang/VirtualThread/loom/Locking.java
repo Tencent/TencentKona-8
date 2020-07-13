675a6a7fc7984e9a760ec393864279b4dc844d9d
emoryzheng <emoryzheng@tencent.com>
2020-07-13 17:00:43 +0800

    Feature(KonaFiber): Pre-defined thread group can not be destroyed and transplant test case about locking, parking and monitoring from loom.
     1.Pre-defined two threads group which are virtual thread group and thread group when thread is created running on virtual thread can not be destroyed.
     2.Transplant test case about locking, parking and monitoring from loom.
    
     Contributed-by:emoryzheng
    
         modified:   jdk/src/share/classes/java/lang/Thread.java
         modified:   jdk/src/share/classes/java/lang/ThreadGroup.java
         new file:   jdk/test/java/lang/VirtualThread/loom/Locking.java
         new file:   jdk/test/java/lang/VirtualThread/loom/Parking.java
         new file:   jdk/test/java/lang/VirtualThread/loom/Monitoring.java
         new file:   jdk/test/java/lang/VirtualThread/loom/TestHelper.java

---
+++ jdk/test/java/lang/VirtualThread/loom/Locking.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * @test
+ * @run testng Locking
+ * @summary Test virtual threads using java.util.concurrent locks
+ */
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+@Test
+public class Locking {
+
+    // lock/unlock
+    public void testReentrantLock1() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            ReentrantLock lock = new ReentrantLock();
+            assertFalse(lock.isHeldByCurrentThread());
+            lock.lock();
+            assertTrue(lock.isHeldByCurrentThread());
+            lock.unlock();
+            assertFalse(lock.isHeldByCurrentThread());
+        });
+    }
+
+    // tryLock/unlock
+    public void testReentrantLock2() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            ReentrantLock lock = new ReentrantLock();
+            assertFalse(lock.isHeldByCurrentThread());
+            boolean acquired = lock.tryLock();
+            assertTrue(acquired);
+            assertTrue(lock.isHeldByCurrentThread());
+            lock.unlock();
+            assertFalse(lock.isHeldByCurrentThread());
+        });
+    }
+
+    // lock/lock/unlock/unlock
+    public void testReentrantLock3() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            ReentrantLock lock = new ReentrantLock();
+            assertFalse(lock.isHeldByCurrentThread());
+            assertTrue(lock.getHoldCount() == 0);
+            lock.lock();
+            assertTrue(lock.isHeldByCurrentThread());
+            assertTrue(lock.getHoldCount() == 1);
+            lock.lock();
+            assertTrue(lock.isHeldByCurrentThread());
+            assertTrue(lock.getHoldCount() == 2);
+            lock.unlock();
+            assertTrue(lock.isHeldByCurrentThread());
+            assertTrue(lock.getHoldCount() == 1);
+            lock.unlock();
+            assertFalse(lock.isHeldByCurrentThread());
+            assertTrue(lock.getHoldCount() == 0);
+        });
+    }
+
+    // locked by dinoasur thread, virtual thread tries to lock
+    public void testReentrantLock4() throws Exception {
+        ReentrantLock lock = new ReentrantLock();
+        AtomicBoolean holdsLock = new AtomicBoolean();
+
+        Thread thread;
+
+        // thread acquires lock
+        lock.lock();
+        try {
+            thread = Thread.startVirtualThread(() -> {
+                lock.lock();  // should block
+                holdsLock.set(true);
+                LockSupport.park();
+                lock.unlock();
+                holdsLock.set(false);
+            });
+            // give time for virtual thread to block
+            Thread.sleep(500);
+            assertFalse(holdsLock.get());
+        } finally {
+            lock.unlock();
+        }
+
+        // virtual thread should acquire lock, park, unpark, and then release lock
+        while (!holdsLock.get()) {
+            Thread.sleep(20);
+        }
+        LockSupport.unpark(thread);
+        while (holdsLock.get()) {
+            Thread.sleep(20);
+        }
+        thread.join();
+    }
+
+    // locked by virtual thread, dinoasur thread tries to lock
+    public void testReentrantLock5() throws Exception {
+        ReentrantLock lock = new ReentrantLock();
+        Thread thread = Thread.startVirtualThread(() -> {
+            lock.lock();
+            try {
+                LockSupport.park();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        // wat for virtual thread to acquire lock
+        while (!lock.isLocked()) {
+            Thread.sleep(20);
+        }
+
+        // thread cannot acquire lock
+        try {
+            assertFalse(lock.tryLock());
+        } finally {
+            // virtual thread should unlock
+            LockSupport.unpark(thread);
+
+            // thread should be able to acquire lock
+            lock.lock();
+            lock.unlock();
+            thread.join();
+        }
+    }
+
+    // lock by virtual thread, another virtual thread tries to lock
+    public void testReentrantLock6() throws Exception {
+        ReentrantLock lock = new ReentrantLock();
+        Thread thread1 = Thread.startVirtualThread(() -> {
+            lock.lock();
+            try {
+                LockSupport.park();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        // wat for virtual thread to acquire lock
+        while (!lock.isLocked()) {
+            Thread.sleep(10);
+        }
+
+        AtomicBoolean holdsLock  = new AtomicBoolean();
+        Thread thread2 = Thread.startVirtualThread(() -> {
+            lock.lock();
+            holdsLock.set(true);
+            LockSupport.park();
+            lock.unlock();
+            holdsLock.set(false);
+        });
+
+        // virtual thread2 should block
+        Thread.sleep(1000);
+        assertFalse(holdsLock.get());
+
+        // unpark virtual thread1
+        LockSupport.unpark(thread1);
+
+        // virtual thread2 should acquire lock
+        while (!holdsLock.get()) {
+            Thread.sleep(20);
+        }
+        // unpark virtual thread and it should release lock
+        LockSupport.unpark(thread2);
+        while (holdsLock.get()) {
+            Thread.sleep(20);
+        }
+
+        thread1.join();
+        thread2.join();
+    }
+}
---
+++ jdk/test/java/lang/VirtualThread/loom/Parking.java
@@ -0,0 +1,297 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * @test
+ * @run testng Parking
+ * @summary Test virtual threads using park/unpark
+ */
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+@Test
+public class Parking {
+
+    // virtual thread parks, unparked by dinosaur thread
+    public void testPark1() throws Exception {
+        Thread thread = Thread.startVirtualThread(LockSupport::park);
+        Thread.sleep(1000); // give time for virtual thread to park
+        LockSupport.unpark(thread);
+        thread.join();
+    }
+
+    // virtual thread parks, unparked by another virtual thread
+    public void testPark2() throws Exception {
+        Thread thread1 = Thread.startVirtualThread(LockSupport::park);
+        Thread.sleep(1000); // give time for virtual thread to park
+        Thread thread2 = Thread.startVirtualThread(() -> LockSupport.unpark(thread1));
+        thread1.join();
+        thread2.join();
+    }
+
+    // park while holding monitor
+    public void testPark3() throws Exception {
+        Thread thread = Thread.startVirtualThread(() -> {
+            Object lock = new Object();
+            synchronized (lock) {
+                LockSupport.park();
+            }
+        });
+        Thread.sleep(1000); // give time for virtual thread to park
+        LockSupport.unpark(thread);
+        thread.join();
+    }
+
+    /*
+
+    // park with native frame on the stack
+    public void testPark4() throws Exception {
+        var virtual thread = virtual threadScope.background().schedule(() -> {
+            try {
+                Method m = Basic.class.getDeclaredMethod("doPark");
+                m.invoke(null);
+            } catch (Exception e) {
+                assertTrue(false);
+            }
+        });
+        Thread.sleep(1000); // give time for virtual thread to park
+        LockSupport.unpark(virtual thread);
+        virtual thread.join();
+    }
+    static void doPark() {
+        LockSupport.park();
+    }
+
+    */
+
+    // unpark before park
+    public void testPark5() throws Exception {
+        Thread thread = Thread.startVirtualThread(() -> {
+            LockSupport.unpark(Thread.currentThread());
+            LockSupport.park();
+        });
+        thread.join();
+    }
+
+    // 2 x unpark before park
+    public void testPark6() throws Exception {
+        Thread thread = Thread.startVirtualThread(() -> {
+            Thread me = Thread.currentThread();
+            LockSupport.unpark(me);
+            LockSupport.unpark(me);
+            LockSupport.park();
+            LockSupport.park();  // should park
+        });
+        Thread.sleep(1000); // give time for virtual thread to park
+        LockSupport.unpark(thread);
+        thread.join();
+    }
+
+    // 2 x park
+    public void testPark7() throws Exception {
+        Thread thread = Thread.startVirtualThread(() -> {
+            LockSupport.park();
+            LockSupport.park();
+        });
+
+        Thread.sleep(1000); // give time for virtual thread to park
+
+        // unpark, virtual thread should park again
+        LockSupport.unpark(thread);
+        Thread.sleep(1000);
+        assertTrue(thread.isAlive());
+
+        // let it terminate
+        LockSupport.unpark(thread);
+        thread.join();
+    }
+
+    // park with interrupt status set
+    public void testPark8() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            t.interrupt();
+            LockSupport.park();
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // interrupt when parked
+    public void testPark9() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            TestHelper.scheduleInterrupt(t, 1000);
+            LockSupport.park();
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // park (pinned park) with interrupt status set
+    public void testPark10() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            t.interrupt();
+            Object lock = new Object();
+            synchronized (lock) {
+                LockSupport.park();
+            }
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // interrupt when parked (pinned park)
+    public void testPark11() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            TestHelper.scheduleInterrupt(t, 1000);
+            Object lock = new Object();
+            synchronized (lock) {
+                LockSupport.park();
+            }
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // parkNanos(-1) completes immediately
+    public void testParkNanos1() throws Exception {
+        TestHelper.runInVirtualThread(() -> LockSupport.parkNanos(-1));
+    }
+
+    // parkNanos(0) completes immediately
+    public void testParkNanos2() throws Exception {
+        TestHelper.runInVirtualThread(() -> LockSupport.parkNanos(0));
+    }
+
+    // parkNanos(1000ms) completes quickly
+    public void testParkNanos3() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            // park for 1000ms
+            long nanos = TimeUnit.NANOSECONDS.convert(1000, TimeUnit.MILLISECONDS);
+            long start = System.nanoTime();
+            LockSupport.parkNanos(nanos);
+
+            // check that virtual thread parks for >= 900ms
+            long elapsed = TimeUnit.MILLISECONDS.convert(System.nanoTime() - start,
+                    TimeUnit.NANOSECONDS);
+            assertTrue(elapsed >= 900);
+        });
+    }
+
+    // virtual thread parks, unparked by dinosaur thread
+    public void testParkNanos4() throws Exception {
+        Thread thread = Thread.startVirtualThread(() -> {
+            long nanos = TimeUnit.NANOSECONDS.convert(30, TimeUnit.SECONDS);
+            LockSupport.parkNanos(nanos);
+        });
+        Thread.sleep(1000); // give time for virtual thread to park
+        LockSupport.unpark(thread);
+        thread.join();
+    }
+
+    // virtual thread parks, unparked by another virtual thread
+    public void testParkNanos5() throws Exception {
+        Thread thread1 = Thread.startVirtualThread(() -> {
+            long nanos = TimeUnit.NANOSECONDS.convert(30, TimeUnit.SECONDS);
+            LockSupport.parkNanos(nanos);
+        });
+        Thread.sleep(1000);  // give time for virtual thread to park
+        Thread thread2 = Thread.startVirtualThread(() -> LockSupport.unpark(thread1));
+        thread1.join();
+        thread2.join();
+    }
+
+    // unpark before parkNanos
+    public void testParkNanos6() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            LockSupport.unpark(Thread.currentThread());
+            long nanos = TimeUnit.NANOSECONDS.convert(30, TimeUnit.SECONDS);
+            LockSupport.parkNanos(nanos);
+        });
+    }
+
+    // unpark before parkNanos(0), should consume permit
+    // not support because we do not have 
+    // boolean join(Duration);
+    /*public void testParkNanos7() throws Exception {
+        Thread thread = Thread.startVirtualThread(() -> {
+            LockSupport.unpark(Thread.currentThread());
+            LockSupport.parkNanos(0);
+            LockSupport.park(); // should block
+        });
+        boolean isAlive = thread.join(Duration.ofSeconds(2));
+        assertTrue(isAlive);
+        LockSupport.unpark(thread);
+        thread.join();
+    }*/
+
+    // parkNanos with interrupt status set
+    public void testParkNanos8() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            t.interrupt();
+            LockSupport.parkNanos(Duration.ofDays(1).toNanos());
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // interrupt when parked in parkNanos
+    public void testParkNanos9() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            TestHelper.scheduleInterrupt(t, 1000);
+            LockSupport.parkNanos(Duration.ofDays(1).toNanos());
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // parkNanos (pinned park) with interrupt status set
+    public void testParkNanos10() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            t.interrupt();
+            Object lock = new Object();
+            synchronized (lock) {
+                LockSupport.parkNanos(Duration.ofDays(1).toNanos());
+            }
+            assertTrue(t.isInterrupted());
+        });
+    }
+
+    // interrupt when parked in parkNanos (pinned park)
+    public void testParkNanos11() throws Exception {
+        TestHelper.runInVirtualThread(() -> {
+            Thread t = Thread.currentThread();
+            TestHelper.scheduleInterrupt(t, 1000);
+            Object lock = new Object();
+            synchronized (lock) {
+                LockSupport.parkNanos(Duration.ofDays(1).toNanos());
+            }
+            assertTrue(t.isInterrupted());
+        });
+    }
+}
