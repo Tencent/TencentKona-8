e0344e87fe5712a7d527110a79ce5c2d4b3ea5c0
seanwxiang <seanwxiang@tencent.com>
2021-09-01 14:07:57 +0800

    FIX(*): Unify the open source version and internal version code
    Summary:
    1. clean up unused code.
    2. adjust the format and date
    
    Conflicts:
    	jdk/test/ProblemList.txt

--- hotspot/src/share/vm/classfile/sharedPathsMiscInfo.cpp
+++ hotspot/src/share/vm/classfile/sharedPathsMiscInfo.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -140,9 +140,10 @@ bool SharedPathsMiscInfo::check(jint type, const char* path_to_check) {
         if (!read_time(&timestamp) || !read_long(&filesize)) {
           return fail("Corrupted archive file header");
         }
-        if (timestamp != st.st_mtime) {
-          return fail("Timestamp mismatch");
-        }
+        // default CDS can move meta_index into other folder, so the timestamp check is invalid.
+        // if (timestamp != st.st_mtime) {
+        //   return fail("Timestamp mismatch");
+        // }
         if (filesize != st.st_size) {
           return fail("File size mismatch");
         }
