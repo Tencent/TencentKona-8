0e67c51dd10983fc0a1aa2948ea438a0717f436c
John Jiang <johnsjiang@tencent.com>
2023-02-09 16:24:59 +0800

    CHORE: Fix KonaJDK Fiber 8 Copyright Notes
    --bug=107228863

--- jdk/test/java/lang/VirtualThread/VTInterruptTest.java
+++ jdk/test/java/lang/VirtualThread/VTInterruptTest.java
@@ -1,23 +1,20 @@
 /*
+ * Copyright (C) 2020, 2023, THL A29 Limited, a Tencent company. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
- * DO NOT ALTER OR REMOVE NOTICES OR THIS FILE HEADER.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
  *
- * This code is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation. THL A29 Limited designates
- * this particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * This code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License version 2 for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 /*
@@ -225,7 +222,7 @@ public class VTInterruptTest {
             }
         };
 
-        Thread vt = Thread.ofVirtual().scheduler(executor).name("vt-0").unstarted(target); 
+        Thread vt = Thread.ofVirtual().scheduler(executor).name("vt-0").unstarted(target);
 
         vt.start();
         while (enter_lock == false) {
@@ -239,4 +236,3 @@ public class VTInterruptTest {
         executor.shutdown();
     }
 }
-
