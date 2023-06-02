d1a4f6eba509f378012c900f9262732399afb9b4
Hui Shi <kalinshi@tencent.com>
2023-06-02 11:05:23 +0800

    Fix(KonaFiber): Split CompensateALot into two tests, relief stress test

--- jdk/test/java/lang/VirtualThread/stress/CompensateALot.java
+++ jdk/test/java/lang/VirtualThread/stress/CompensateALot.java
@@ -59,18 +59,27 @@ public class CompensateALot {
         assertEquals(((ForkJoinPool)e).getPoolSize(), 0);
 
         Thread vt_locked = Thread.ofVirtual().scheduler(e).name("vt_locked").start(target);
-        while (locked != true);
+        while (locked != true) {
+            Thread.sleep(5);
+        }
 
         assertEquals(((ForkJoinPool)e).getPoolSize(), 1);
         Thread[] vts = new Thread[200];
         for (int i = 0; i < 200; i++) {
+            System.out.println("launching vt" + i);
             vts[i] = Thread.ofVirtual().scheduler(e).name("vt", i).start(target);
-            while (vts[i].getState() != Thread.State.BLOCKED);
+        }
+
+        for (int i = 0; i < 200; i++) {
+            while (vts[i].getState() != Thread.State.BLOCKED) {
+                Thread.sleep(3);
+            }
+            System.out.println("vt" + i + " in block status");
         }
 
         while (((ForkJoinPool)e).getPoolSize() < 200) {
             System.out.println("1: pool size is " + ((ForkJoinPool)e).getPoolSize());
-            Thread.sleep(100);
+            Thread.sleep(10);
         }
         assertTrue(((ForkJoinPool)e).getPoolSize() >= 200);
 
@@ -86,75 +95,7 @@ public class CompensateALot {
 
         while (((ForkJoinPool)e).getPoolSize() != 0) {
             System.out.println("pool size is " + ((ForkJoinPool)e).getPoolSize());
-            Thread.sleep(1000);
-        }
-
-        e.shutdown();
-        e.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-    }
-
-    @Test
-    /*
-     * 1. vt_locked hold synchronized lock and wait, at the same time,
-     *    create a new worker thread of ForkJoinPool.
-     * 2. vt0~vt199 hold synchronized lock and wait.(if vt0 do not create
-     *    a new worker thread before block, there is no carrier thread
-     *    to run vt1, main thread will spin to check state of vt1 forever)
-     * 3. Main thread invoke notifyAll to wake up vt.
-     * 4. All vt finish normally.
-     * 5. Wait thread number of ForkJoinPool back to zero.
-     */
-    public static void syncWaitCompensate() throws Exception {
-        ExecutorService e = Executors.newWorkStealingPool(1);
-        ThreadFactory tf = Thread.ofVirtual().scheduler(e).name("vt", 0).factory();
-
-        Object o = new Object();
-        Runnable target = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (o) {
-                    System.out.println("get lock " + Thread.currentThread());
-                    try {
-                        o.wait();
-                    }
-                    catch (InterruptedException e) {
-                    }
-                }
-            }
-        };
-
-        assertEquals(((ForkJoinPool)e).getPoolSize(), 0);
-
-        Thread vt_locked = Thread.ofVirtual().scheduler(e).name("vt_locked").start(target);
-        while (vt_locked.getState() != Thread.State.WAITING);
-
-        while (((ForkJoinPool)e).getPoolSize() < 2) {
-            Thread.sleep(100);
-        }
-        assertEquals(((ForkJoinPool)e).getPoolSize(), 2);
-
-        Thread[] vts = new Thread[200];
-        for (int i = 0; i < 200; i++) {
-            vts[i] = Thread.ofVirtual().scheduler(e).name("vt", i).start(target);
-        }
-
-        while (((ForkJoinPool)e).getPoolSize() < 201) {
-            System.out.println("pool is " + (ForkJoinPool)e);
-            Thread.sleep(200);
-        }
-        assertTrue(((ForkJoinPool)e).getPoolSize() >= 201);
-
-        synchronized (o) {
-            o.notifyAll();
-        }
-
-        while (((ForkJoinPool)e).getPoolSize() > 0) {
-            Thread.sleep(1000);
-        }
-
-        vt_locked.join();
-        for (int i = 0; i < 200; i++) {
-            vts[i].join();
+            Thread.sleep(10);
         }
 
         e.shutdown();
